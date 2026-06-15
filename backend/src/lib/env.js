79e50b7839b7580dddb5ff44ad329f63e7e249cb
Inba <inbavarunans@gmail.com>
2026-06-15 14:43:21 +0530

    feat: implemented smtp instead of resend

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -8,10 +8,12 @@ export const ENV = {
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
 
-  RESEND_API_KEY: process.env.RESEND_API_KEY,
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_PORT: process.env.SMTP_PORT,
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASS: process.env.SMTP_PASS,
+
   CLIENT_URL: process.env.CLIENT_URL,
-  EMAIL_FROM: process.env.EMAIL_FROM,
-  EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
 
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
